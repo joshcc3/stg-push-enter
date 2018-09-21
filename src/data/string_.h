700776e06c46c65fbcf8f8c7bf0d81dea9662200
Joshua Coutinho <jcoutin@loydjcoutin1.intra.gsacapital.com>
2018-09-21 15:44:38 +0100

    Added my own string type

---
+++ src/data/string_.h
@@ -0,0 +1,22 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include "err.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct string_ {
+  char *char_arr;
+  int length;
+};
+
+
+err_t init_string_(const char *, int, struct string_ **);
+err_t deinit_string_(struct string_ **);
+
+int string_length(const struct string_ *);
+char charAt(const struct string_ *, int);
+err_t substring_(const struct string_ *, int, int, struct string_ **);
+bool equals(const struct string_ *, const struct string_ *);
+
+#endif
---
+++ src/stg/lang/lang.h
@@ -0,0 +1,20 @@
+
+err_t var
+err_t lit
+err_t atom
+err_t expr
+err_t func_call
+err_t primop
+err_t let
+err_t case_st
+
+err_t case_alt
+err_t case_alt_con
+err_t case_alt_default
+
+err_t fun
+err_t pap
+err_t con
+err_t thunk
+
+
