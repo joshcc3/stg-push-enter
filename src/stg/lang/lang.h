12fd07b22eb081dd31aa0e9fd37baed4328992ef
Joshua Coutinho <jcoutin@loydjcoutin1.intra.gsacapital.com>
2018-09-21 16:02:39 +0100

    Added definition of the language src/stg/lang.h

--- src/stg/lang/lang.h
+++ src/stg/lang/lang.h
@@ -1,20 +1,26 @@
+#ifndef LANG_H
+#define LANG_H
 
-err_t var
-err_t lit
-err_t atom
+#include "data/string_.h"
+#include "containers/resizable_array.h"
+
+err_t var(string_, union atom*);
+err_t lit_int(int, union atom*);
+err_t lit_double(double, union atom *);
 err_t expr
-err_t func_call
-err_t primop
-err_t let
-err_t case_st
+err_t func_call(string_, int, union atom, union expr *);
+err_t primop(string_, int, union atom, union expr *);
+err_t atom(union atom, union expr *);
+err_t let(string_, union object, union expr *, union expr *); 
+err_t case_st(union expr *, union alt *, union expr *);
 
-err_t case_alt
-err_t case_alt_con
-err_t case_alt_default
+// Only allows variables for the second argument
+err_t case_alt_con(string_, union atom *, union expr *, union alt *);
+err_t case_alt_default(string_, union expr*, union alt *);
 
-err_t fun
-err_t pap
-err_t con
-err_t thunk
+err_t fun(union atom *, union expr *, union object *);
+err_t pap(string_, union atom *, union object *);
+err_t con(string_, union atom *, union object *);
+err_t thunk(union expr *, union object *);
 
-
+#endif
