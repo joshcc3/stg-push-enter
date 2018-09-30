611974c45f55cda499a92c5c007cb24773209ce6
jrc12 <jrc12@ic.ac.uk>
2018-09-30 22:31:17 +0100

    Began implementation for a code generator in haskell

--- haskell/CodeGen.hs
+++ haskell/CodeGen.hs
@@ -1,2 +1,16 @@
 
 
+eval (L (I x)) = show x
+eval (V x) = x
+eval (Let s o e) = do
+            ref <- createThunk "bindings" o
+            bind s ref
+            eval e
+eval (Case e alts) = do
+  pushCaseFrame contName updateid "bindings"
+  returnCaseCont e alts
+eval (Primop "+#" [L (I x), L (I y)]) = show (x + y)
+eval (Primop "+#" [V a, z]) = eval (Case (V a) alts)
+    where
+      alts = [Alt "I#" [newVar] 
+            
