27d617ab3f213aa452cb4485153fed9149848f30
Coutinho, Joshua <Joshua.Coutinho@gsacapital.com>
2019-07-16 21:10:56 +0100

    Add parser and parser test

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,66 +2,70 @@ module Parser where
 
 import Text.Parsec.Language 
 import Text.Parsec.Token as TP
+import Text.Parsec
+import Text.Parsec.Combinator
 import Types
-import Control.Applicative
+import Control.Applicative(liftA2)
 
 stgDef :: LanguageDef st
 stgDef = haskellStyle {
-    reservedOpNames =  ["|", "=", "->", "+#", "-#", "==#", ">#", "<#"],
-    reservedNames = ["data", "let", "in", "case", "of"]
+    reservedOpNames =  ["|", "=", "->"],
+    reservedNames = ["data", "let", "in", "case", "of", "Boxed", "Unboxed"]
 }
+
 stg = makeTokenParser stgDef
 program = Program <$> many1 conDecl <*> many1 funDef
-funDef = (,) <$> (whiteSpace *> TP.identifier) <*> object
+funDef = (,) <$> (TP.identifier stg <* reservedOp stg "=") <*> object
 conDecl = ConDecl <$> 
-    (whiteSpace *> string "data" *> TP.identifier <* string "=" <* whiteSpace) <*>
-    (many1 conDefn <* string "|" <* whiteSpace)
+    (reserved stg "data" *> TP.identifier stg <* reservedOp stg "=") <*>
+    (zipWith toDefn [0..] <$> (many1 conDefn <* reservedOp stg "|" ))
   where
-    conDefn = (,) <$> TP.identifier <*> many1 field
-    field = fmap f TP.identifier 
-    f x = if take (length "Boxed") x == "Boxed" then (x, Boxed) else (x, Unboxed)
+    toDefn ix (n, fs) = ConDefn n ix fs
+    conDefn = (,) <$> TP.identifier stg <*> many1 field
+    field = fmap f $ TP.identifier stg
+    f x = if take (length "Unboxed") x == "Unboxed" then (x, Boxed) else (x, Unboxed)
 
 object = FUNC <$> function  <|> CON <$> constructor <|>  PAP <$> pap <|> THUNK <$> thunk 
-constructor = Con <$> TP.identifier <*> many1 TP.identifier
-pap = PAP <$> (TP.identifier <* string "$!" <* whiteSpace) <*> many1 atom
+constructor = Con <$> TP.identifier stg <*> many1 atom
+pap = Pap <$> (TP.identifier stg <* reservedOp stg "$!" ) <*> many1 atom
 thunk = expr
-function = Fun <$> (string "\\" *> whiteSpace *> many1 arg) <*> expr
+function = Fun <$> (reservedOp stg "\\" *> many1 arg <* reservedOp stg "->") <*> expr
   where
-    arg = TP.parens ((,) <$> (TP.identifier <* whiteSpace <* string "," <* whiteSpace) <*> TP.identifier)
+    arg = TP.parens stg ((,) <$> (TP.identifier stg) <*> typ)
+    typ = (Boxed <$ reserved stg "Boxed") <|> (Unboxed <$ reserved stg "Unboxed")
 
-lit = (L . fromInteger) <$> TP.integer
-var = V <$> TP.identifier
-primop = liftA2 P TP.operator . liftA2 (:) atom $ fmap (:[]) atom 
-atom = lit <|> var <|> primop
+lit = (L . fromInteger) <$> TP.integer stg
+var = V <$> TP.identifier stg
+primop = P <$> (parens stg  helper <|> helper)
+    where
+        helper = liftA2 Primop (TP.operator stg) . liftA2 (:) atom $ fmap (:[]) atom
+atom = try primop <|> lit <|> var
 
-expr = (Atom <$> atom) <|> letExpr <|> caseExpr <|> funcExpr
-letExpr = Let <$> 
-  (whiteSpace *> string "let" *> whiteSpace *> TP.identifier <* whiteSpace) <*> 
-  (object <* whiteSpace) <*> 
-  (expr <* whiteSpace)
-caseExpr = AltCase <$> 
-    (whiteSpace *> string "case" whiteSpace) <*> 
-    (atom <* whiteSpace <* string "of" <* whiteSpace) <*> 
+expr = try caseExpr <|> try letExpr <|> try funcCallExpr <|> (Atom <$> try atom)
+letExpr = Let <$>
+  (reserved stg "let" *> TP.identifier stg <* reservedOp stg "=") <*>
+  (object) <*>
+  (reserved stg "in" *> expr)
+caseExpr = Case <$> (reserved stg "case" *> atom <* reserved stg "of") <*>
     alts
-
-alts = many1 alt
   where
-    alt = Alt <$> TP.identifier <*> (many1 TP.identifier) <*> expr
+    alts = many1 alt
+    alt = AltCase <$> TP.identifier stg <*> (many1 (TP.identifier stg)) <*>
+        (reservedOp stg "->" *> expr)
+
+funcCallExpr = FuncCall <$> TP.identifier stg <*> many1 atom
 
 
-
-
-
-main = let one = I# 1
-       let inc = THUNK (plus_int one)
-       let inced = THUNK (map inc list)
-       let list = Cons one inced
-       let element1 = THUNK (head inced)
-       let tail1 = THUNK (tail inced)
-       let element2 = THUNK (head tail1)
-       in case plus_int element1 element2 of
-          I# z -> print_int z
-
-(identifier <* whiteSpace) 
-
-expression = 
+-- main = let one = I# 1
+--        let inc = THUNK (plus_int one)
+--        let inced = THUNK (map inc list)
+--        let list = Cons one inced
+--        let element1 = THUNK (head inced)
+--        let tail1 = THUNK (tail inced)
+--        let element2 = THUNK (head tail1)
+--        in case plus_int element1 element2 of
+--           I# z -> print_int z
+--
+-- (identifier <* whiteSpace)
+--
+-- expression =
---
+++ src/ParserTest.hs
@@ -0,0 +1,48 @@
+module ParserTest where
+
+import Parser
+import Text.Parsec
+import Types
+import Text.Parsec.Token
+
+
+t1 = ("1", lit, L 1)
+
+t2 = ("case x of\n  Pair a b -> +# a b",
+      caseExpr,
+      Case (V "x") [AltCase "Pair" ["a","b"] (Atom (P (Primop "+#" [V "a",V "b"])))]
+      )
+
+t3 = ("let one = I 1 in one", letExpr, Let "one" (CON (Con "I" [L 1])) (Atom (V "one")))
+
+t4 = ("let one = I 1 in \n\
+      \ let inc = plus_int one in \n \
+      \ let inced = map inc list in \n \
+      \ let list = Cons one inced in \n \
+      \ let element1 = head inced in \n \
+      \ let tail1 = tail inced in \n \
+      \ let element2 = head tail1 in \n \
+      \ let element3 = plus_int element1 element2 in \n \
+      \ case element3 of \n \
+      \   I z -> print_int z",
+      letExpr,
+      Let "one" (CON (Con "I" [L 1])) (Let "inc" (CON (Con "plus_int" [V "one"])) (Let "inced" (CON (Con "map" [V "inc",V "list"])) (Let "list" (CON (Con "Cons" [V "one",V "inced"])) (Let "element1" (CON (Con "head" [V "inced"])) (Let "tail1" (CON (Con "tail" [V "inced"])) (Let "element2" (CON (Con "head" [V "tail1"])) (Let "element3" (CON (Con "plus_int" [V "element1",V "element2"])) (Case (V "element3") [AltCase "I" ["z"] (FuncCall "print_int" [V "z"])])))))))))
+
+t5 = ("plus_int = \\ (x1 Boxed) (y1 Boxed) -> case x1 of \n\
+        \ I a1 -> case y1 of\n\
+        \ I b1 -> let c1 = I (+# a1 b1)\n\
+        \ in c1",
+      funDef,
+      ("plus_int",FUNC (Fun [("x1",Boxed),("y1",Boxed)] (Case (V "x1") [AltCase "I" ["a1"] (Case (V "y1") [AltCase "I" ["b1"] (Let "c1" (CON (Con "I" [P (Primop "+#" [V "a1",V "b1"])])) (Atom (V "c1")))])])))
+      )
+
+
+t6 = "data Int = I UnboxedIVal"
+
+t7 = "data Pair = Pa P_fst P_snd"
+
+t8 = "data List = Cons C_element C_next"
+
+t9 = "data Unit = Unit"
+
+t10 = "data Bool = True | False"
